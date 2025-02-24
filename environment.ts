9fc2147cd9e8d6610037267fcfffcf3126b8a9b5
kathir-2002 <kathirmathavan17@gmail.com>
2025-02-24 23:33:15 +0530

    setup for dev branch

--- environment.ts
+++ environment.ts
@@ -1,5 +1,5 @@
 export const config = {
   // apiUrldb: 'http://172.17.0.55:8000/',
-  apiUrldb: 'http://172.17.0.111:8000/',
+  apiUrldb: 'http://192.168.35.105:8000/',
   // apiUrldb: 'https://montra-api.onrender.com/',
 };
